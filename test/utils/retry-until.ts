9e1986d56dca1e81cdc59f914c06b98cbcfb02f4
Harjot Singh <harjot@harkul.com>
2020-11-20 15:35:08 +0000

    test: fix retry-until test utility to only loop on failure

--- test/utils/retry-until.ts
+++ test/utils/retry-until.ts
@@ -1,5 +1,4 @@
 import { wait } from './wait'
 
 export const retryUntil = ( fn: () => Promise<void> ) => fn()
-  .catch( () => wait( 1000 ) )
-  .then( fn )
+  .catch( () => wait( 1000 ).then( fn ) )
