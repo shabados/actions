26552282334c2b3dbc858ae467227c89ed43b6bc
Harjot Singh <harjot@harkul.com>
2021-03-14 02:18:37 +0000

    test(publish-github): add integration cases and nock mocks

--- publish-github/index.integration.spec.ts
+++ publish-github/index.integration.spec.ts
@@ -1,17 +1,108 @@
-import '.'
+import { join } from 'path'
+import { chdir } from 'process'
+
+import { mkdirp, writeFile } from 'fs-extra'
+import nock from 'nock'
+import { v4 } from 'uuid'
+
+import { setWith } from '../test/utils'
+
+const run = () => import( '.' ).then( ( { default: run } ) => run() )
+
+type NockReleaseBody = { prerelease: boolean, tag_name: string, body: string }
+const nockCreateRelease = ( body?: NockReleaseBody, response?: nock.Body ) => nock( 'https://api.github.com' )
+  .post( `/repos/${process.env.GITHUB_REPOSITORY!}/releases`, body )
+  .reply( 200, response )
+
+const nockUploadAsset = ( releaseId: number, name: string ) => nock( 'https://uploads.github.com' )
+  .post( `/repos/${process.env.GITHUB_REPOSITORY!}/releases/${releaseId}/assets?name=${name}&` )
+  .reply( 200 )
+
+const gitMock = {
+  push: jest.fn(),
+  raw: jest.fn( () => 'v1.0.0' ),
+}
+
+jest.mock( 'simple-git', () => () => gitMock )
+
+const TMP_PATH = join( __dirname, 'tmp' )
 
 describe( 'publish-github', () => {
   describe( 'when pushing', () => {
-    it.todo( 'should push the contents of the directory to the main branch' )
+    it( 'should push the contents of the directory to the main branch', async () => {
+      const testBranch = 'test'
+      setWith( { main_branch: testBranch } )
+
+      nockCreateRelease()
+
+      await run()
+
+      expect( gitMock.push ).toHaveBeenCalledWith( 'origin', testBranch, { '--follow-tags': null } )
+    } )
   } )
 
   describe( 'when creating a release', () => {
-    it.todo( 'should create a release with the latest tag' )
+    it( 'should create a release with the latest tag', async () => {
+      setWith()
+
+      const latestTag = 'v1.3.0'
+      gitMock.raw.mockReturnValue( latestTag )
+
+      const createRelease = nockCreateRelease( { body: '', tag_name: 'v1.3.0', prerelease: false } )
+
+      await run()
+
+      expect( createRelease.isDone() ).toBeTruthy()
+    } )
   } )
 
   describe( 'when uploading assets', () => {
-    it.todo( 'should upload all assets, given globs via asset_paths' )
-    it.todo( 'should upload a changelog, if it exists' )
-    it.todo( 'should not upload a changelog, if it does not exist' )
+    it( 'should upload all assets, given globs via asset_paths', async () => {
+      setWith( {
+        asset_paths: `
+        test*
+        **/*.png
+        `,
+      } )
+      const mockFiles = [ 'test1.txt', 'test.txt', 'test3.text', 'asset1.png', 'ignore.txt' ]
+
+      const path = join( TMP_PATH, v4() )
+      await mkdirp( path )
+      chdir( path )
+      await Promise.all( mockFiles.map( ( name ) => writeFile( name, '' ) ) )
+
+      const responseId = 1
+      nockCreateRelease( undefined, { id: responseId } )
+      const uploadAssets = mockFiles
+        .slice( 0, 4 )
+        .map( ( name ) => nockUploadAsset( responseId, name ) )
+
+      await run()
+
+      expect( uploadAssets.every( ( mock ) => mock.isDone() ) ).toBeTruthy()
+    } )
+
+    it( 'should upload a changelog, if it exists', async () => {
+      setWith( { changelog_path: 'changelog.md' } )
+
+      const path = join( TMP_PATH, v4() )
+      await mkdirp( path )
+      chdir( path )
+
+      const latestTag = 'v1.1.0'
+      gitMock.raw.mockReturnValue( latestTag )
+      const changelogContent = 'test changelog \nnew change'
+      await writeFile( 'changelog.md', changelogContent )
+
+      const createRelease = nockCreateRelease( {
+        body: changelogContent,
+        tag_name: latestTag,
+        prerelease: false,
+      } )
+
+      await run()
+
+      expect( createRelease.isDone() ).toBeTruthy()
+    } )
   } )
 } )
