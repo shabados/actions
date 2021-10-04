5bab9b4bda7f51aac4b770be83714516f83c3bdd
Harjot Singh <harjot@harkul.com>
2021-10-04 03:14:00 +0100

    fix(publish-github): disable notify on merged PR
    Incorrectly called and breaks tests

--- publish-github/notify.ts
+++ publish-github/notify.ts
@@ -4,15 +4,16 @@ type Notify = {
   octokit: ReturnType<typeof getOctokit>,
   version: string,
   releaseLink: string,
+  issueNumber: string,
 }
 
-const notify = async ( { octokit, releaseLink, version }: Notify ) => {
-  const { issue: { number, owner, repo } } = context
+const notify = async ( { issueNumber, octokit, releaseLink, version }: Notify ) => {
+  const { issue: { owner, repo } } = context
 
   await octokit.issues.createComment( {
-    body: `Released as [${version}](${releaseLink}) 🎉`,
+    body: `Released in [${version}](${releaseLink}) 🎉`,
     // eslint-disable-next-line @typescript-eslint/naming-convention
-    issue_number: number,
+    issue_number: issueNumber,
     owner,
     repo,
   } )
