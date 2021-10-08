018980232a52b30332222e1c46b5d11f4918b2f7
Harjot Singh <harjot@harkul.com>
2021-10-08 20:26:27 +0100

    fix(publish-branches): filter null release branches out
    Disallows release/ from being published

--- publish-branch/get-branches.ts
+++ publish-branch/get-branches.ts
@@ -1,6 +1,12 @@
 import { getInput } from '@actions/core'
 import { major, minor, prerelease } from 'semver'
 
+const getReleaseBranches = ( version: string ) => [
+  'latest',
+  `v${major( version )}`,
+  `v${major( version )}.${minor( version )}`,
+]
+
 // Enumerate the release branches to push to
 const getBranches = async () => {
   const prefix = getInput( 'release_branch_prefix' )
@@ -8,25 +14,23 @@ const getBranches = async () => {
   // Get latest version through input
   const version = getInput( 'release_version' )
 
-  // Optionally get the name of the prerelease branch
-  const prereleaseBranch = getInput( 'prerelease_branch' )
+  const [ prereleaseName ] = prerelease( version ) ?? []
 
-  const [ prereleaseName ] = prerelease( version ) || []
+  // Use the supplied prerelease branch name, otherwise extract it from the version
+  const prereleaseBranch = getInput( 'prerelease_branch' ) || prereleaseName
+
+  const isRelease = !prereleaseName
 
   return [
     version,
-    ...( prereleaseName
-      // Use the supplied prerelease branch name, otherwise extract it from the version
-      ? [ prereleaseBranch ?? prereleaseName ]
-      : [
-        'latest',
-        `v${major( version )}`,
-        `v${major( version )}.${minor( version )}`,
-        // If defined, update the prerelease branch
-        ...( prereleaseBranch ? [ prereleaseBranch ] : [] ),
-      ]
-    ),
-  ].map( ( suffix ) => `${prefix}/${suffix}` )
+    // Always update the prerelease branch, if supplied
+    prereleaseBranch,
+    ...( isRelease ? getReleaseBranches( version ) : [] ),
+  ]
+    // Remove any falsy values
+    .filter( ( x ) => x )
+    // Add release prefix
+    .map( ( suffix ) => `${prefix}/${suffix}` )
 }
 
 export default getBranches
